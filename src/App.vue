9d7afe1c53180552d6bc7c58bfe69ce42156ec29
AntonMiracle <b.anton.m.1986@gmail.com>
2021-04-17 13:44:33 +0200

    add component search

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
   <div id="app">
+    <search></search>
   </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
 import search from '@/components/search';
+
 
 export default {
   name: 'App',
   components: {
     search
-  }
+  },
+  computed: mapGetters(['getSearchValue'])
 }
 </script>
 
 <style>
+* {
+  transition: all .1s;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
 }
+
 </style>
---
+++ src/components/search.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="search">
+    <span> {{ error }}</span>
+    <form @submit.prevent="startSearch">
+      <input type="text" v-model="searchValue" placeholder="enter word or phrase">
+      <button type="submit">find</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import {mapMutations} from "vuex";
+
+export default {
+  methods: {
+    ...mapMutations(["setSearchValue"]),
+    startSearch() {
+      console.log('START SEARCH');
+      if (this.searchValue.trim()) {
+        this.setSearchValue(this.searchValue);
+        this.searchValue = this.error = '';
+      } else {
+        this.error = "can not be empty";
+      }
+    }
+  },
+  data() {
+    return {
+      searchValue: '',
+      error: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+:root {
+  --m-width: 45px
+}
+
+.search {
+  padding: 15px 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+span {
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  font-size: .8em;
+}
+
+input {
+  outline: none;
+  padding: 5px 10px;
+  width: var(--m-width);
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, .2);
+  margin-right: 10px;
+}
+
+input:focus {
+  box-shadow: 0 1px 0 0 darkslategrey;
+}
+
+input::placeholder {
+  opacity: .5;
+}
+
+button {
+  outline: none;
+  border: 1px solid #2c3e50;
+  padding: 5px 15px;
+  width: var(--m-width);
+  background-color: azure;
+}
+
+button:active {
+  background-color: #2c3e50;
+  color: azure;
+}
+</style>
